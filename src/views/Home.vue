8c98de33baf52b018d58bc4b2c514427898e25d2
maru0pengin <maru0pengin@gmail.com>
2021-03-23 01:24:12 +0900

    tsをつかうvueのサンプルプログラムにpixijsを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,35 @@
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
+import * as PIXI from "pixi.js"; // node_modulesから PIXI.jsをインポート
+import * as PIXI_SOUND from "pixi-sound";// node_modulesから PIXI_SOUNDをインポート
+
+PIXI_SOUND.default.init();
+
+// PIXI.JSアプリケーションを呼び出す (この数字はゲーム内の画面サイズ)
+const app = new PIXI.Application({ width: 400, height: 600 });
+
+// index.htmlのbodyにapp.viewを追加する (app.viewはcanvasのdom要素)
+document.body.appendChild(app.view);
+
+// ゲームcanvasのcssを定義する
+// ここで定義した画面サイズ(width,height)は実際に画面に表示するサイズ
+app.renderer.view.style.position = "relative";
+app.renderer.view.style.width = "400px";
+app.renderer.view.style.height = "600px";
+app.renderer.view.style.display = "block";
+
+// canvasの周りを点線枠で囲う (canvasの位置がわかりやすいので入れている)
+app.renderer.view.style.border = "3px dashed #ffcccc";
+
+// canvasの背景色
+app.renderer.backgroundColor = 0xdeb887;
+
+// 一座標確認に使用
+app.view.addEventListener('pointermove', (ev) => {
+    console.log(ev.clientX, ev.clientY);
+});
+
 @Options({
   components: {
     HelloWorld,
