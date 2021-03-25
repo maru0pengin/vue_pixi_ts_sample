e4a8bd58dbe49d1e2c569b4be3cc57853c0ee2c2
maru0pengin <maru0pengin@gmail.com>
2021-03-25 20:57:03 +0900

    home画面からゲームプレイ画面への遷移

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
-  <main id="app">
+  <main id="campus">
     <!-- ここにPixiの描画領域(Canvas)が入る -->
     test
   </main>
@@ -14,36 +14,6 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 import * as PIXI from "pixi.js"; // node_modulesから PIXI.jsをインポート
 import * as PIXI_SOUND from "pixi-sound"; // node_modulesから PIXI_SOUNDをインポート
-PIXI_SOUND.default.init();
-
-// PIXI.JSアプリケーションを呼び出す (この数字はゲーム内の画面サイズ)
-const app = new PIXI.Application({
-  width: 400,
-  height: 600,
-});
-
-// HTMLの<main id="app"></main>の中に上で作ったPIXIアプリケーション(app)のビュー(canvas)を突っ込む(app.viewはcanvasのdom要素)
-let el: any = document.getElementById("app");
-el.appendChild(app.view);
-//document.body.appendChild(app.view);
-
-// ゲームcanvasのcssを定義する
-// ここで定義した画面サイズ(width,height)は実際に画面に表示するサイズ
-app.renderer.view.style.position = "relative";
-app.renderer.view.style.width = "400px";
-app.renderer.view.style.height = "600px";
-app.renderer.view.style.display = "block";
-
-// canvasの周りを点線枠で囲う (canvasの位置がわかりやすいので入れている)
-app.renderer.view.style.border = "3px dashed #ffcccc";
-
-// canvasの背景色
-app.renderer.backgroundColor = 0xdeb887;
-
-// 一座標確認に使用
-app.view.addEventListener("pointermove", (ev) => {
-  console.log(ev.clientX, ev.clientY);
-});
 
 @Options({
   //アノテーション
@@ -51,12 +21,46 @@ app.view.addEventListener("pointermove", (ev) => {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class About extends Vue {
+  mounted() {
+    console.log("aiueo");
+    PIXI_SOUND.default.init();
+
+    // PIXI.JSアプリケーションを呼び出す (この数字はゲーム内の画面サイズ)
+    const app = new PIXI.Application({
+      width: 400,
+      height: 600,
+    });
+
+    // HTMLの<main id="app"></main>の中に上で作ったPIXIアプリケーション(app)のビュー(canvas)を突っ込む(app.viewはcanvasのdom要素)
+    let el: any = document.getElementById("campus");
+    el.appendChild(app.view);
+    //document.body.appendChild(app.view);
+
+    // ゲームcanvasのcssを定義する
+    // ここで定義した画面サイズ(width,height)は実際に画面に表示するサイズ
+    app.renderer.view.style.position = "relative";
+    app.renderer.view.style.width = "400px";
+    app.renderer.view.style.height = "600px";
+    app.renderer.view.style.display = "block";
+
+    // canvasの周りを点線枠で囲う (canvasの位置がわかりやすいので入れている)
+    app.renderer.view.style.border = "3px dashed #ffcccc";
+
+    // canvasの背景色
+    app.renderer.backgroundColor = 0xdeb887;
+
+    // 一座標確認に使用
+    app.view.addEventListener("pointermove", (ev) => {
+      console.log(ev.clientX, ev.clientY);
+    });
+  }
+}
 </script>
 
 <style>
 main {
   margin-left: 100px;
-  background-color: black;
+  background-color: red;
 }
 </style>
